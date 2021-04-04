17cbd8e5917dd2a69913e192433b933f6f03a9ae
Jonathan Henrique <jhowjhoe@icloud.com>
2021-04-04 17:37:23 -0300

    Update index.py

--- index.py
+++ index.py
@@ -7,9 +7,9 @@ import urllib.parse
 import requests
 
 app = flask.Flask(__name__)
-app.config['DEBUG'] = True
+app.config['DEBUG'] = False
 key = "d6f49dee38b888"
-port = 5100
+port = 81
 
 @app.route('/geocode', methods=['GET'])
 def geocode():
